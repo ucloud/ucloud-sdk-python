c4ead70e0eb3c4d681dc513c76115843195f7cd5
yanheven <yanheven@gmail.com>
2015-03-19 23:55:24 +0800

    fix python code style

--- sdk.py
+++ sdk.py
@@ -1,61 +1,63 @@
-#-*- encoding: utf-8 -*-
-import hashlib,json,httplib
+# -*- coding: utf-8 -*-
+import hashlib, json, httplib
 import urlparse
 import urllib
 import sys
 from config import *
 
+
 class UCLOUDException(Exception):
     def __str__(self):
         return "Error"
 
+
 def _verfy_ac(private_key, params):
-    items=params.items()
+    items = params.items()
     items.sort()
 
-    params_data = "";
+    params_data = ""
     for key, value in items:
         params_data = params_data + str(key) + str(value)
 
     params_data = params_data+private_key
-
+    print params_data
     '''use sha1 to encode keys'''
     hash_new = hashlib.sha1()
     hash_new.update(params_data)
     hash_value = hash_new.hexdigest()
     return hash_value
 
+
 class UConnection(object):
     def __init__(self, base_url):
         self.base_url = base_url
         o = urlparse.urlsplit(base_url)
         if o.scheme == 'https':
-            self.conn = httplib.HTTPSConnection(o.netloc);
+            self.conn = httplib.HTTPSConnection(o.netloc)
         else:
-            self.conn = httplib.HTTPConnection(o.netloc);
+            self.conn = httplib.HTTPConnection(o.netloc)
 
     def __del__(self):
-        self.conn.close();
-
+        self.conn.close()
 
     def get(self, resouse, params):
         resouse += "?" + urllib.urlencode(params)
         print("%s%s" % (self.base_url, resouse))
-        self.conn.request("GET", resouse);
-        response = json.loads(self.conn.getresponse().read());
-        return response;
+        self.conn.request("GET", resouse)
+        response = json.loads(self.conn.getresponse().read())
+        return response
+
 
 class UcloudApiClient(object):
     # 添加 设置 数据中心和  zone 参数
     def __init__(self, base_url, public_key, private_key):
-        self.g_params = {};
-        self.g_params['PublicKey'] = public_key;
-        self.private_key = private_key;
-        self.conn = UConnection(base_url);
-
+        self.g_params = {}
+        self.g_params['PublicKey'] = public_key
+        self.private_key = private_key
+        self.conn = UConnection(base_url)
 
     def get(self, uri, params):
-         #print params
-        _params =  dict(self.g_params, **params);
-        _params["Signature"] = _verfy_ac(self.private_key, _params);
-        return self.conn.get(uri, _params);
+        # print params
+        _params = dict(self.g_params, **params)
+        _params["Signature"] = _verfy_ac(self.private_key, _params)
+        return self.conn.get(uri, _params)

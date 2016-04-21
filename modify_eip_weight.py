0be813d471815969d8ef9adbc58e47be462cfcf3
wentianle <wentianle@ucloud.cn>
2016-04-21 18:28:08 +0800

    添加调整EIP 权重API示范

---
+++ modify_eip_weight.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from sdk import UcloudApiClient
+from config import *
+import sys
+import json
+
+#实例化 API 句柄
+
+
+if __name__=='__main__':
+    arg_length = len(sys.argv)
+    ApiClient = UcloudApiClient(base_url, public_key, private_key)
+    Parameters={
+            "Action":"ModifyEIPWeight",
+            "EIPId":"eip-womesr",
+            "Region":"cn-north-03"
+            "Weight": 1
+            }
+    response = ApiClient.get("/", Parameters);
+    print response;

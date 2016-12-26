1a12df10e415b4315161b09ca1aff267db9f933d
nigel <nigel.nming@gmail.com>
2016-12-26 23:52:19 +0800

    sdk support http post

--- sdk.py
+++ sdk.py
@@ -47,6 +47,13 @@ class UConnection(object):
         response = json.loads(self.conn.getresponse().read())
         return response
 
+    def post(self, uri, params):
+        print("%s%s %s" % (self.base_url, uri, params))
+        headers = {"Content-Type": "application/json"}
+        self.conn.request("POST", uri, json.JSONEncoder().encode(params), headers)
+        response = json.loads(self.conn.getresponse().read())
+        return response
+
 
 class UcloudApiClient(object):
     # 添加 设置 数据中心和  zone 参数
@@ -65,3 +72,12 @@ class UcloudApiClient(object):
 
         _params["Signature"] = _verfy_ac(self.private_key, _params)
         return self.conn.get(uri, _params)
+
+    def post(self, uri, params):
+        _params = dict(self.g_params, **params)
+
+        if project_id :
+            _params["ProjectId"] = project_id
+
+        _params["Signature"] = _verfy_ac(self.private_key, _params)
+        return self.conn.post(uri, _params)
